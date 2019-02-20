5bc72ad25a8f39555e1c2188f20be85625da94e0
smallnest <smallnest@gmail.com>
2019-02-20 15:33:42 +0800

    add workerpool

---
+++ 8_server_workerpool/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/rcrowley/go-metrics"
+)
+
+var (
+	c = flag.Int("c", 10, "concurrency")
+)
+var (
+	opsRate = metrics.NewRegisteredMeter("ops", nil)
+)
+
+var epoller *epoll
+var workerPool *pool
+
+func main() {
+	flag.Parse()
+
+	setLimit()
+	go metrics.Log(metrics.DefaultRegistry, 5*time.Second, log.New(os.Stderr, "metrics: ", log.Lmicroseconds))
+
+	ln, err := net.Listen("tcp", ":8972")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatalf("pprof failed: %v", err)
+		}
+	}()
+
+	workerPool = newPool(*c, 1000000)
+
+	epoller, err = MkEpoll()
+	if err != nil {
+		panic(err)
+	}
+
+	go start()
+
+	for {
+		conn, e := ln.Accept()
+		if e != nil {
+			if ne, ok := e.(net.Error); ok && ne.Temporary() {
+				log.Printf("accept temp err: %v", ne)
+				continue
+			}
+
+			log.Printf("accept err: %v", e)
+			return
+		}
+
+		if err := epoller.Add(conn); err != nil {
+			log.Printf("failed to add connection %v", err)
+			conn.Close()
+		}
+	}
+
+	workerPool.Close()
+}
+
+func start() {
+	for {
+		connections, err := epoller.Wait()
+		if err != nil {
+			log.Printf("failed to epoll wait %v", err)
+			continue
+		}
+		for _, conn := range connections {
+			if conn == nil {
+				break
+			}
+
+			workerPool.addTask(conn)
+		}
+	}
+}
+
+func setLimit() {
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+
+	log.Printf("set cur limit: %d", rLimit.Cur)
+}
---
+++ 8_server_workerpool/worker_pool.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+type pool struct {
+	workers   int
+	maxTasks  int
+	taskQueue chan net.Conn
+
+	mu     sync.Mutex
+	closed bool
+	done   chan struct{}
+}
+
+func newPool(w int, t int) *pool {
+	return &pool{
+		workers:   w,
+		maxTasks:  t,
+		taskQueue: make(chan net.Conn, t),
+		done:      make(chan struct{}),
+	}
+}
+
+func (p *pool) Close() {
+	p.mu.Lock()
+	p.closed = true
+	close(p.done)
+	close(p.taskQueue)
+	p.mu.Unlock()
+}
+
+func (p *pool) addTask(conn net.Conn) {
+	p.mu.Lock()
+	if p.closed {
+		p.mu.Unlock()
+		return
+	}
+	p.mu.Unlock()
+
+	p.taskQueue <- conn
+}
+
+func (p *pool) start() {
+	for i := 0; i < p.workers; i++ {
+		go p.startWorker()
+	}
+}
+
+func (p *pool) startWorker() {
+	for {
+		select {
+		case <-p.done:
+			return
+		case conn := <-p.taskQueue:
+			if conn != nil {
+				handleConn(conn)
+
+			}
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	_, err := io.CopyN(conn, conn, 8)
+	if err != nil {
+		if err := epoller.Remove(conn); err != nil {
+			log.Printf("failed to remove %v", err)
+		}
+	}
+	opsRate.Mark(1)
+}
