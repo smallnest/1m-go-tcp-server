55d4ba7e0d0605e4f43cec5666d3a19fab72f375
smallnest <smallnest@gmail.com>
2019-02-21 14:09:13 +0800

    add io-intensive server

---
+++ 10_ io_intensive_epoll_server/server.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/libp2p/go-reuseport"
+	"github.com/rcrowley/go-metrics"
+)
+
+var (
+	c      = flag.Int("c", 10, "concurrency")
+	iotime = flag.Duration("io", time.Duration(10*time.Second), "sleep time")
+)
+
+var (
+	opsRate = metrics.NewRegisteredMeter("ops", nil)
+)
+
+func main() {
+	flag.Parse()
+
+	setLimit()
+	go metrics.Log(metrics.DefaultRegistry, 5*time.Second, log.New(os.Stderr, "metrics: ", log.Lmicroseconds))
+
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatalf("pprof failed: %v", err)
+		}
+	}()
+
+	for i := 0; i < *c; i++ {
+		go startEpoll()
+	}
+
+	select {}
+}
+
+func startEpoll() {
+	ln, err := reuseport.Listen("tcp", ":8972")
+	if err != nil {
+		panic(err)
+	}
+
+	epoller, err := MkEpoll()
+	if err != nil {
+		panic(err)
+	}
+
+	go start(epoller)
+
+	for {
+		conn, e := ln.Accept()
+		if e != nil {
+			if ne, ok := e.(net.Error); ok && ne.Temporary() {
+				log.Printf("accept temp err: %v", ne)
+				continue
+			}
+
+			log.Printf("accept err: %v", e)
+			return
+		}
+
+		if err := epoller.Add(conn); err != nil {
+			log.Printf("failed to add connection %v", err)
+			conn.Close()
+		}
+	}
+}
+
+func start(epoller *epoll) {
+	for {
+		connections, err := epoller.Wait()
+		if err != nil {
+			log.Printf("failed to epoll wait %v", err)
+			continue
+		}
+		for _, conn := range connections {
+			if conn == nil {
+				break
+			}
+
+			time.Sleep(*iotime)
+			io.CopyN(conn, conn, 8)
+			if err != nil {
+				if err := epoller.Remove(conn); err != nil {
+					log.Printf("failed to remove %v", err)
+				}
+				conn.Close()
+			}
+
+			opsRate.Mark(1)
+		}
+	}
+}
+
+func setLimit() {
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+
+	log.Printf("set cur limit: %d", rLimit.Cur)
+}
